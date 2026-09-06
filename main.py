98a4390ed601c4b30f828a4ae3f9e3eb3de0ca1b
novatralabs-lab <novatralabs@gmail.com>
2026-09-07 03:25:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 # CORS settings
 origins = [
-    "https://test-ebon-gamma-lqkcicmgjt.vercel.app/",  # Sab origins allow — production mein isko specific domains tak limit karein
+    "https://test-ebon-gamma-lqkcicmgjt.vercel.app",  # Sab origins allow — production mein isko specific domains tak limit karein
 ]
 
 app.add_middleware(
